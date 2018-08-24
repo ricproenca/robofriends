8e92c23df418ec05b70a89356bcc7e61c3bb58ab
Ricardo Proenca <ext.ricardo.proenca@ingg.com>
2018-08-24 16:40:20 +0100

    fix lighthouse errors

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,9 +4,10 @@ const SearchBox = ({ searchChange }) => {
   return (
     <div className="tc">
       <input
+        aria-label="Search Robots"
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
-        placeholder="search robot"
+        placeholder="Search Robots"
         onChange={searchChange}
       />
     </div>
